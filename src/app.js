9d32658917bca47369a25cada112df6f3bbcb962
kaue05 <kaueoliveiragck@gmail.com>
2025-11-24 19:37:50 -0400

    adicionado nome

--- src/app.js
+++ src/app.js
@@ -95,10 +95,7 @@ const html = `
   </head>
   <body>
     <section>
-      VPS - Hello Express API V 1.3.4
-    </section>
-    <section>
-      by Kaue
+      VPS - Hello Express API V 1.3.4 by Kaue
     </section>
   </body>
 </html>
