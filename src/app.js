8f65da45a8d110652c1e997219ca0b5cb8dfcf35
kaue05 <kaueoliveiragck@gmail.com>
2025-11-28 21:05:44 -0400

    alteração no nome

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ const html = `
   </head>
   <body>
     <section>
-      VPS - Hello Express API V 1.3.4 by Kaue
+      VPS - Hello Express API V 1.3.4 by Kaue.Santos
     </section>
   </body>
 </html>
